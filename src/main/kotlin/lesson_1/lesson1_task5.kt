cb1df78882faff94c2887ff7a569f5fd1c6993e0
euakulov <eu.akulov0@gmail.com>
2024-12-26 03:32:42 +0400

    KS-1-task5

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+//Секунды – 6480
+
+fun main(){
+    val second = 6480
+    val minute = second / 60
+    val hour = minute / 60
+    print(hour)
+    print(":")
+    print(minute - (hour * 60))
+    print(":")
+    print("00")
+}
