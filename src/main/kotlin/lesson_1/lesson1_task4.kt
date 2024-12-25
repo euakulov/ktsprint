f368606d42bbb5762364bab969a39134aa94a36f
euakulov <eu.akulov0@gmail.com>
2024-12-26 02:05:44 +0400

    KS-1-task4

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_1
+
+/*Данные для задачи:
+Расстояние 40_868_600_000
+Возраст – 27
+Часть дня – 0.075
+Секунды – 6480
+Часть года – 2.0547945205479453E-4
+Апогей орбиты – 327000*/
+
+fun main(){
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Int = 6480
+    val partOfYear: Float = 2.0547945205479453E-4f
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
+
